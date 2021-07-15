b45dc4568b17a03c8caa612959c286513c65476b
Quentin Brillais <qbrillai@c1r4p3.42nice.fr>
2021-07-15 14:31:14 +0200

    Removed files and folders

--- ft_isdigit.c
+++
@@ -1,9 +0,0 @@
-#include "cub3d.h"
-
-int	ft_isdigit(char i)
-{
-	if (i >= '0' && i <= '9')
-		return (1);
-	else
-		return (0);
-}
--- ft_paramsprite.c
+++
@@ -1,9 +0,0 @@
-#include "cub3d.h"
-
-int	ft_spriteparam(t_game *game)
-{
-	game->param.ZBuffer = malloc(sizeof(double) * game->param.screenWidth);
-	game->param.spriteOrder = malloc(sizeof(int) * game->param.sprite);
-	game->param.spriteDistance = malloc(sizeof(double) * game->param.sprite);
-	return (1);
-}
