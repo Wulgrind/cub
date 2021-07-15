d6ce53eaedbe4c4665bc30bd7da063c7b875a026
Quentin Brillais <qbrillai@c1r4p3.42nice.fr>
2021-07-15 16:30:13 +0200

    new commit

--- ft_checkMap.c
+++ ft_checkMap.c
@@ -6,7 +6,7 @@
 /*   By: qbrillai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 14:13:40 by qbrillai          #+#    #+#             */
-/*   Updated: 2021/07/15 16:15:52 by qbrillai         ###   ########.fr       */
+/*   Updated: 2021/07/15 16:29:30 by qbrillai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,14 +37,16 @@ int	ft_checkMap3(t_game *game)
 	col = 0;
 	while (row < game->param.row - 1)
 	{
-		if (game->map[row][0] != 1 && game->map[row][0] != 9 && game->map[row][0] != 0)
+		if (game->map[row][0] != 1 && game->map[row][0]
+						!= 9 && game->map[row][0] != 0)
 			return (0);
 		while (game->map[row][col] != 10)
 		{
 			col++;
 		}
 		col--;
-		if (game->map[row][col] != 1 && game->map[row][col] != 9 && game->map[row][0] != 0)
+		if (game->map[row][col] != 1 && game->map[row][col]
+						!= 9 && game->map[row][0] != 0)
 			return (0);
 		col = 0;
 		row++;
