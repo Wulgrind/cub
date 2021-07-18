cfa70957efd5b8dfe2a6b7c39c4f7b9925b234e6
Wulgrind <wulgrind@MacBook-Pro-de-Wulgrind.local>
2021-07-18 09:38:52 -0700

    new commit

--- loop2.c
+++ loop2.c
@@ -18,7 +18,7 @@ int	ft_loop6(t_game *game)
 	game->step = 1.0 * game->img.img_height / game->lineHeight;
 	game->texPos = (game->drawStart - game->param.screenHeight
 			/ 2 + game->lineHeight / 2) * game->step;
-	while (game->y++ < game->drawEnd)
+	while (game->y < game->drawEnd)
 	{
 		game->texY = (int)game->texPos & (game->img.img_height - 1);
 		game->texPos += game->step;
@@ -26,7 +26,9 @@ int	ft_loop6(t_game *game)
 			* game->img.img_width + game->texX];
 		game->display.data[game->y * game->param.screenWidth
 			+ game->x] = game->color;
+		game->y++;
 	}
+	mlx_destroy_image(game->mlx, game->img.img)
 	game->move.moveSpeed = 0.2;
 	game->move.rotSpeed = 0.2;
 	return (1);
