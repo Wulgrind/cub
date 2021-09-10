1441268918bce8636c46c32c5f9565bd6c2c4e0d
Quentin Brillais <qbrillai@c3r2p2.42nice.fr>
2021-09-10 18:02:04 +0200

    new commit

--- ft_chkOrder.c
+++ ft_chkOrder.c
@@ -6,7 +6,7 @@
 /*   By: qbrillai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 14:11:41 by qbrillai          #+#    #+#             */
-/*   Updated: 2021/09/07 10:54:40 by qbrillai         ###   ########.fr       */
+/*   Updated: 2021/09/10 18:01:23 by qbrillai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ void	ft_chkorder(t_game *game)
 	if (game->param.no == NULL
 		|| game->param.so == NULL
 		|| game->param.we == NULL
-		|| game->param.ea == NULL
-		|| (game->param.f[0] == 0 && game->param.f[1] == 0
-			&& game->param.f[2] == 0)
-		|| (game->param.f[0] == 0 && game->param.f[1] == 0
-			&& game->param.f[2] == 0))
+		|| game->param.ea == NULL)
 		game->param.wrong++;
 }
